0092a5edefff9932ab35f48c7b0f77d1697cdc0d
seansophea <sonsophea777@gmail.com>
2015-10-07 11:50:18 +0700

    Update ChiTietDe.java
    Toi commit 1

--- ThiTracNghiem/src/DuLieu/ChiTietDe.java
+++ ThiTracNghiem/src/DuLieu/ChiTietDe.java
@@ -1,15 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package DuLieu;
 
-/**
- *
- * @author vietthang
- */
+import NghiepVu.Connect;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 public class ChiTietDe {
+   String MaDe;
+    String MS;
     
+    public ChiTietDe(String MaDe, String MS) {
+        this.MaDe = MaDe;
+        this.MS = MS;
+    }
+
+    public ChiTietDe(String MaDe) {
+        this.MaDe = MaDe;
+    }
+
+    public ChiTietDe() {
+    }
+    
+    public ResultSet LayNoiDungDe(String s){
+          ResultSet rs = null;
+         try {
+             PreparedStatement pst = Connect.connection.prepareStatement("select MaDe, ChiTietDe.MS as MaCauHoi,Hard as DoKho  from ChiTietDe,Cauhoi where MaDe=? and ChiTietDe.MS=Cauhoi.MS");
+             pst.setString(1, s);
+             rs = pst.executeQuery();
+         } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Lỗi load nội dung đề!");
+         }
+         return rs;
+      }
+    
+    public ResultSet getdata(){
+        ResultSet rs = null;
+         try {  
+           Statement st = Connect.connection.createStatement();
+           rs = st.executeQuery("select distinct MaDe from ChiTietDe");       
+       } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null,"Lỗi mã đề!");
+       }
+         return rs;
+    }
+    
+    public void XoaDeThi(String made){
+         try {
+             PreparedStatement pst = Connect.connection.prepareStatement("delete from ChiTietDe where MaDe=?");
+             pst.setString(1, made);
+             if(pst.executeUpdate()>0) JOptionPane.showMessageDialog(null, "Delete success đề thi "+made);
+         } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Mã đề thi không tồn tại!");
+         }
+    }
+    public String RandomDe(){
+         String made="";
+         try {
+             ResultSet rs =null;
+             Statement st = Connect.connection.createStatement();
+             rs = st.executeQuery("SELECT TOP 1 MaDe from ChiTietDe ORDER BY NEWID()");
+             while (rs.next()){
+                 made = rs.getString(1);
+                 return made;
+             }
+         } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Lỗi random đề thi!");
+         }
+         return made;
+    }
+    
+    public void ThemDe(String made, String mach){
+         try {
+             int x;
+             PreparedStatement pst = Connect.connection.prepareStatement("insert into ChiTietDe values(?,?)");
+             pst.setString(1, made);
+             pst.setString(2, mach);
+             if(pst.executeUpdate()>0) x=0;
+         } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null,"Câu hỏi đã được thêm rồi!");
+         }
+    }
+    
+    public String getMaDe() {
+        return MaDe;
+    }
+
+    public void setMaDe(String MaDe) {
+        this.MaDe = MaDe;
+    }
+
+    public String getMS() {
+        return MS;
+    }
+
+    public void setMS(String MS) {
+        this.MS = MS;
+    } 
 }
