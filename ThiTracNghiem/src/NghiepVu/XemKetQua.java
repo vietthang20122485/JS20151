6e65c4278f69d63b706af45b4da20106d07cdc9c
seansophea <sonsophea777@gmail.com>
2015-11-05 16:21:59 +0700

    Bắt sự kiện cho nút Xem kết quả

--- ThiTracNghiem/src/NghiepVu/XemKetQua.java
+++ ThiTracNghiem/src/NghiepVu/XemKetQua.java
@@ -6,9 +6,18 @@
 
 package NghiepVu;
 
+import DuLieu.KetQua;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
- * @author vietthang20122485
+ * @author sonsophea777
  */
 public class XemKetQua extends javax.swing.JFrame {
 
@@ -17,6 +26,10 @@ public class XemKetQua extends javax.swing.JFrame {
      */
     public XemKetQua() {
         initComponents();
+    }
+    public KetQua getKetQua(){
+        String mssv = txtMSSV.getText().trim();
+        return new KetQua(mssv);
     }
 
     /**
@@ -29,18 +42,78 @@ public class XemKetQua extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        lblTitle = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        txtMSSV = new javax.swing.JTextField();
+        btnXem = new javax.swing.JButton();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jTable3 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 204));
+
+        lblTitle.setFont(new java.awt.Font("Times New Roman", 1, 36)); // NOI18N
+        lblTitle.setForeground(new java.awt.Color(0, 0, 255));
+        lblTitle.setText("K?t qu? bài thi");
+
+        jLabel1.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+        jLabel1.setText("Nh?p m? s? sinh viên:");
+
+        txtMSSV.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+
+        btnXem.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+        btnXem.setText("Xem đi?m");
+        btnXem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnXemActionPerformed(evt);
+            }
+        });
+
+        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane4.setViewportView(jTable3);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(82, 82, 82)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnXem)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 534, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(lblTitle)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(4, 4, 4)
+                            .addComponent(txtMSSV, javax.swing.GroupLayout.PREFERRED_SIZE, 198, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(34, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblTitle)
+                .addGap(34, 34, 34)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txtMSSV, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(36, 36, 36)
+                .addComponent(btnXem)
+                .addGap(47, 47, 47)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(109, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -57,11 +130,46 @@ public class XemKetQua extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnXemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnXemActionPerformed
+        // TODO add your handling code here:
+        KetQua ketqua = getKetQua();
+        DefaultTableModel dmt = new DefaultTableModel();
+        try {
+            ResultSet rs = ketqua.XemDiem(txtMSSV.getText().trim());
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int col = rsmd.getColumnCount();
+            String [] arr = new String [col];
+            for (int i =0;i<col;i++) {
+                arr[i]=rsmd.getColumnName(i+1);
+            }
+            dmt.setColumnIdentifiers(arr);
+            while(rs.next()){
+                for (int i =0;i<col;i++){
+                    try {
+                        arr[i]= rs.getString(i+1);
+                    } catch (SQLException ex) {
+                        JOptionPane.showMessageDialog(null, "Error!");
+                    }
+                }
+                dmt.addRow(arr);
+            }
+            jTable3.setModel(dmt);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Không t?m th?y thông tin!");
+        }
+    }//GEN-LAST:event_btnXemActionPerformed
+
     /**
      * @param args the command line arguments
      */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnXem;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTable jTable3;
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JTextField txtMSSV;
     // End of variables declaration//GEN-END:variables
 }
